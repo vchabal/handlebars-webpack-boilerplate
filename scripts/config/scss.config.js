ef21f5ae3e2b04d30accd5fe9259b6b6e990df93
Vladimir Chabal <v.chabal@gmail.com>
2018-02-01 00:58:15 +0100

    Compile all scss in src/pages dir

--- scripts/config/scss.config.js
+++ scripts/config/scss.config.js
@@ -1,12 +1,25 @@
+const fs = require('fs');
 const path = require('path');
 const paths = require('./paths.config');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const extractSass = new ExtractTextPlugin('[name]');
 
+const DIR_PROJECT = "../../";
+const DIR_PAGES = path.join(__dirname, DIR_PROJECT, paths.pages);
+const REGEX_SCSS = /\.scss$/;
+
+// Read all files in src/pages folder
+// Pick only *.scss files
+const styles = fs.readdirSync(DIR_PAGES).filter(fileName => REGEX_SCSS.test(fileName));
+const entry = styles.reduce((entriesObject, fileName) => {
+    const css = fileName.replace(REGEX_SCSS, '.css');
+
+    entriesObject[css] = ("./" + path.join(paths.pages, fileName));
+    return entriesObject;
+}, {});
+
 module.exports = {
-    entry: {
-        'styles.css': ("./" + path.join(paths.pages, 'styles.scss'))
-    },
+    entry: entry,
 
     loaders: [
         {
