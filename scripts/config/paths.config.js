c257ce5db1f409bd3a59791e995459a8bd77d42f
Vladimir Chabal <v.chabal@gmail.com>
2018-02-17 23:29:21 +0100

    Paths configuration

--- scripts/config/paths.config.js
+++ scripts/config/paths.config.js
@@ -1,9 +1,10 @@
 const DIR_DIST = 'dist/';
 const DIR_SRC = 'src/';
-const DIR_PAGES = 'src/pages';
-const DIR_HELPERS = 'src/helpers';
-const DIR_PARTIALS = 'src/partials';
-const DIR_SCSS = 'src/scss';
+const DIR_PAGES = DIR_SRC + 'pages';
+const DIR_HELPERS = DIR_SRC + 'helpers';
+const DIR_PARTIALS = DIR_SRC + 'partials';
+const DIR_SCSS = DIR_SRC + 'scss';
+
 module.exports = {
     src: DIR_SRC,
     dist: DIR_DIST,
